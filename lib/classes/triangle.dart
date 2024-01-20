88982b6a05f8dd2ed4f36526ad68692738f82bf3
jvenin <jvenin@drouot.com>
2024-01-20 14:17:19 +0100

    feat: triangle class

--- lib/classes/triangle.dart
+++ lib/classes/triangle.dart
@@ -1,18 +1,46 @@
-import 'package:equatable/equatable.dart';
 import 'package:paraworld_gsf_viewer/classes/vertex.dart';
+import 'package:vector_math/vector_math_64.dart';
 
-class Triangle extends Equatable {
-  const Triangle({
+class ModelTriangle {
+  ModelTriangle({
     required this.points,
-  });
+    required this.indices,
+  }) {
+    assert(points.length == indices.length && points.length == 3);
+  }
 
   final List<Vertex> points;
+  final List<int> indices;
 
-  drawTriangle() {
-    
+  bool shouldShowTriangle({
+    double xRotation = 0,
+    double yRotation = 0,
+    double zRotation = 0,
+  }) {
+    final p0 = points[0].transform(
+        xRotation: xRotation, yRotation: yRotation, zRotation: zRotation);
+    final p1 = points[1].transform(
+        xRotation: xRotation, yRotation: yRotation, zRotation: zRotation);
+    final p2 = points[2].transform(
+        xRotation: xRotation, yRotation: yRotation, zRotation: zRotation);
+    final normal = (p1 - p0).cross(p2 - p0);
+
+    // we place our eye at center far from camera, hence -100 in z
+    return normal.dot(p0 - Vector3(0, 0, -100)) <= 0;
   }
 
   @override
-  // TODO: implement props
-  List<Object?> get props => points;
+  String toString() {
+    return indices.toString();
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      other is ModelTriangle &&
+      other.indices[0] == indices[0] &&
+      other.indices[1] == indices[1] &&
+      other.indices[2] == indices[2];
+
+  @override
+  int get hashCode => indices.hashCode;
 }
