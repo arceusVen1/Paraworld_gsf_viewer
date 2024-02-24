536171d6e5fd8d1f0ed3ac7c10ed653847fc9949
jvenin <jvenin@drouot.com>
2024-02-24 19:00:46 +0100

    fix: tab length

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
               bottom: const TabBar(
