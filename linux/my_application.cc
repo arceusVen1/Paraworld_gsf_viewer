82afdbab9f24312c6f8b08d2632773d7b760601a
Tatsukio <66260493+Tatsukio@users.noreply.github.com>
2024-05-09 19:19:54 +0500

    naming_update

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "paraworld_gsf_viewer");
+    gtk_header_bar_set_title(header_bar, "ParaWorld GSF Viewer");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "paraworld_gsf_viewer");
+    gtk_window_set_title(window, "ParaWorld GSF Viewer");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
